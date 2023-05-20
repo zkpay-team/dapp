a3eaed89e9d2f4814ea659fca762ec5b4bbb946a
0xRomain <romain@talentlayer.org>
2023-05-20 09:31:22 +0200

    fix dynamic class

--- src/components/Layout/MenuBottom.tsx
+++ src/components/Layout/MenuBottom.tsx
@@ -4,7 +4,7 @@ import { navigation } from './navigation';
 function MenuBottom() {
   return (
     <div className='md:hidden fixed bottom-0 left-0 z-50 w-full h-16 border-t bg-zinc-800 border-zinc-600'>
-      <div className={`grid h-full max-w-lg grid-cols-${navigation.length} mx-auto font-medium`}>
+      <div className={`grid h-full max-w-lg grid-cols-5 mx-auto font-medium`}>
         {navigation.map(item => (
           <BottomLink key={item.name} href={item.href}>
             <item.icon className='w-6 h-6 mb-1 ' />
