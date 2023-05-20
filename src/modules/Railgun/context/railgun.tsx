d98ba9b24fc940f95c5cdc0bf9f164ad30e18bcc
0xRomain <romain@talentlayer.org>
2023-05-20 12:40:40 +0200

    add create and load wallet

--- src/modules/Railgun/context/railgun.tsx
+++ src/modules/Railgun/context/railgun.tsx
@@ -1,9 +1,17 @@
-import { createContext, ReactNode, useEffect, useMemo, useState } from 'react';
+import { createContext, ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
 import { useAccount } from 'wagmi';
 
-import { getProver, Groth16 } from '@railgun-community/quickstart';
+import {
+  getProver,
+  Groth16,
+  createRailgunWallet,
+  loadWalletByID,
+} from '@railgun-community/quickstart';
 import { IAccount } from '../../../types';
 import { initializeRailgun, loadProviders } from '../utils/setup';
+import { LoadRailgunWalletResponse } from '@railgun-community/shared-models';
+import { entropyToMnemonic, randomBytes } from 'ethers/lib/utils';
+import { NetworkName } from '@railgun-community/shared-models';
 
 declare global {
   interface Window {
@@ -11,18 +19,27 @@ declare global {
   }
 }
 
+interface localStoreWallet {
+  encryptionKey: string;
+  railgunWalletInfo: {
+    id: string;
+    railgunAddress: string;
+  };
+}
+
 const RailgunContext = createContext<{
   isProviderLoaded: boolean;
   account?: IAccount;
+  createWallet?: () => void;
+  wallet?: LoadRailgunWalletResponse;
 }>({
   isProviderLoaded: false,
-  account: undefined,
 });
 
 const RailgunProvider = ({ children }: { children: ReactNode }) => {
   const account = useAccount();
   const [isProviderLoaded, setProviderLoaded] = useState<boolean>(false);
-  // const [wallet, setWallet] = useState<LoadRailgunWalletResponse>();
+  const [wallet, setWallet] = useState<LoadRailgunWalletResponse>();
 
   console.log('RailgunProvider', { isProviderLoaded });
 
@@ -52,16 +69,64 @@ const RailgunProvider = ({ children }: { children: ReactNode }) => {
 
       // Provider is done loading.
       setProviderLoaded(true);
+
+      // LOAD Wallet if there is one
+      const savedWalletString = localStorage.getItem('wallet');
+      if (savedWalletString) {
+        const savedWallet = JSON.parse(savedWalletString) as localStoreWallet;
+        console.log('savedWallet', savedWallet);
+        if (!savedWallet.encryptionKey || savedWallet.railgunWalletInfo.id) {
+          return;
+        }
+        const railgunWallet = await loadWalletByID(
+          savedWallet.encryptionKey,
+          savedWallet.railgunWalletInfo.id,
+          false,
+        );
+        console.log('loadWalletByID');
+        setWallet(railgunWallet);
+      }
     };
     fn();
+  }, []);
+
+  const createWallet = useCallback(async () => {
+    const mnemonic = entropyToMnemonic(randomBytes(16));
+
+    // Current block numbers for each chain when wallet was first created.
+    // If unknown, provide undefined.
+    const creationBlockNumberMap = {
+      [NetworkName.EthereumGoerli]: undefined,
+    };
+
+    // let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
+
+    const encryptionKey = Buffer.from(randomBytes(32)).toString('hex');
+
+    const railgunWallet = await createRailgunWallet(
+      encryptionKey,
+      mnemonic,
+      creationBlockNumberMap,
+    );
+    console.log({ railgunWallet });
+    setWallet(railgunWallet);
+    localStorage.setItem(
+      'wallet',
+      JSON.stringify({
+        encryptionKey,
+        ...railgunWallet,
+      }),
+    );
   }, []);
 
   const value = useMemo(() => {
     return {
       account: account ? account : undefined,
       isProviderLoaded: isProviderLoaded,
+      createWallet: createWallet,
+      wallet: wallet,
     };
-  }, [account.address, isProviderLoaded]);
+  }, [account.address, isProviderLoaded, wallet]);
 
   return <RailgunContext.Provider value={value}>{children}</RailgunContext.Provider>;
 };
