af8595fb110b6b20288b7b729136628b79698bd6
Berend-Jan Lange <berendjanlange@Berend-Jans-MacBook-Pro.local>
2023-05-20 18:08:57 +0200

    modified groups

--- src/pages/groups.tsx
+++ src/pages/groups.tsx
@@ -1,9 +1,13 @@
+import GroupsComponent from '../components/Groups/GroupsComponent';
+
 function Groups() {
   return (
     <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
-      <p className='text-3xl font-medium tracking-wider mb-8'>
+      <p className='text-3xl font-medium tracking-wider mb-8 font-space-grotesk-light'>
         Manage <span className='text-zinc-600'>groups </span>
       </p>
+
+      <GroupsComponent />
     </div>
   );
 }
