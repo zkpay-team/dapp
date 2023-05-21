1cd345b4a3e3404d589c32e90c4b12c1be02b16c
0xRomain <romain@talentlayer.org>
2023-05-21 10:58:49 +0200

    install 1inch & create deposti page

---
+++ src/pages/deposit.tsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import Steps from '../components/Steps';
+import RailgunContext from '../modules/Railgun/context/railgun';
+
+function Deposit() {
+  const { wallet, account } = useContext(RailgunContext);
+
+  if (!account?.isConnected || !wallet?.railgunWalletInfo) {
+    return <Steps />;
+  }
+
+  return (
+    <div className='max-w-7xl mx-auto text-gray-200 sm:px-4 lg:px-0'>
+      <p className='text-3xl font-medium mb-8'>
+        Swap <span className='text-gray-100'>and shield </span>
+      </p>
+
+      <div className='mt-4'></div>
+    </div>
+  );
+}
+
+export default Deposit;
