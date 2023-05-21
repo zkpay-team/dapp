5133621a36bbb990277779fe7215e973acce5629
0xRomain <romain@talentlayer.org>
2023-05-21 11:34:46 +0200

    update swap form

--- src/pages/deposit.tsx
+++ src/pages/deposit.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import Steps from '../components/Steps';
 import RailgunContext from '../modules/Railgun/context/railgun';
+import SwapForm from '../modules/1inch/components/SwapForm';
 
 function Deposit() {
   const { wallet, account } = useContext(RailgunContext);
@@ -12,10 +13,12 @@ function Deposit() {
   return (
     <div className='max-w-7xl mx-auto text-gray-200 sm:px-4 lg:px-0'>
       <p className='text-3xl font-medium mb-8'>
-        Swap <span className='text-gray-100'>and shield </span>
+        Swap <span className='text-gray-100'>and shield</span>
       </p>
 
-      <div className='mt-4'></div>
+      <div className='mt-4'>
+        <SwapForm />
+      </div>
     </div>
   );
 }
